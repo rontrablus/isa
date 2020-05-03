07b673fd42afb57c78b8e1948477d74cf8c5711b
Matan Achiel <MATANACHIEL@MAIL.TAU.AC.IL>
2020-05-03 19:53:03 +0300

    yotam

---
+++ isa project/isa project/const.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+char *a = "yotam";
--- isa project/isa project/isa1.c
+++ isa project/isa project/isa1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <const.h>
 int main()
 {
 	printf("hello");
 	printf("ron");
 	printf("matan");
+	printf("%s", a);
 	return 0;
 }
